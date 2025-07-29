1cd7eef655ce76afe2308e5ddd3b67d4b1f9cca7
Iwan Francis <iwanfrancis@gmail.com>
2025-07-29 16:38:30 +0100

    refactor: refactor Button component

--- frontend/src/components/inputs/button/Button.tsx
+++ frontend/src/components/inputs/button/Button.tsx
@@ -1,26 +1,25 @@
 import { cn } from '@/utils/cn'
+import { forwardRef } from 'react'
 
-type ButtonProps = {
-  onClick?: (...args: any[]) => void
-  children: string
-  buttonType?: 'submit' | 'reset' | 'button'
-  disabled?: boolean
-}
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ({ onClick, children, buttonType, disabled }: ButtonProps) => {
-  return (
-    <button
-      type={buttonType}
-      className={cn(
-        'border-0 rounded-lg bg-pink text-white px-2.5 py-1.5 font-semibold cursor-pointer transition-colors duration-200 ease-in-out hover:bg-dark-pink',
-        { 'opacity-50 cursor-not-allowed hover:bg-pink': disabled }
-      )}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  )
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          'border-0  rounded-lg bg-pink text-white px-2.5 py-1.5 font-semibold cursor-pointer transition-colors duration-200 ease-in-out hover:bg-dark-pink',
+          { 'opacity-50 cursor-not-allowed hover:bg-pink': disabled },
+          className
+        )}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+)
 
 export default Button
